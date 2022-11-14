bf0e234ced903919584f3053c44be0c98a60c26e
patri <patrick.ma864@gmail.com>
2022-11-14 15:11:52 -0500

    initial Site methods

---
+++ src/ResidentialSite.java
@@ -0,0 +1,8 @@
+public class ResidentialSite {
+
+    private double getBillableAmount(double _units, double _rate){
+        double base = _units * _rate;
+        double tax = base * Site.TAX_RATE;
+        return base + tax;
+    }
+}
